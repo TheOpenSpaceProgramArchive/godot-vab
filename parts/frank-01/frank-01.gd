a3e795d86664082ad8586b73c526dff5d57f9406
jeff <jeff@legnd.com>
2017-07-31 11:08:30 -0500

    No idea what I'm doing.

---
+++ parts/frank-01/frank-01.gd
@@ -0,0 +1,13 @@
+extends "res://parts/part.gd"
+
+# The RayCast2D length of the part.
+var ray_length = 100.0 # px
+
+func _draw():
+	var from = Vector2(0.0, ray_length) # px
+	var to = Vector2(0.0, -ray_length) # px
+	var color = Color(1.0, 1.0, 1.0) # white
+	draw_line(from, to, color)
+
+func _ready():
+	print("whatever")
