ff224fe68a45ce8db8839ea37a554db31245f6f5
jeff <jeff@legnd.com>
2017-07-31 15:22:07 -0500

    coffee break

--- parts/part.gd
+++ parts/part.gd
@@ -1,9 +1,12 @@
 extends RigidBody2D
 
-# The size of the collision shape.
-onready var points = get_node("CollisionPolygon2D").get_polygon()
+# The points of the collision polygon.
+onready var points = get_node("CollisionPolygon2D").get_polygon() # Vector2Array
 
-# This is dumb.
+# The size of the RayCast2D.
+var ray_size = 50.0 # px
+
+# This is dumb...
 # Return the lowest y coordinate of the collision shape.
 var ys = []
 var ymax = 0
@@ -26,8 +29,16 @@ func highest_y_coord():
 				ymin = y
 	return ymin
 
-########
-########
+# Draw the RayCast2D.
+func _draw():
+	var from = Vector2(0.0, lowest_y_coord())
+	var to = Vector2(0.0, lowest_y_coord() + ray_size)
+	var color = Color(1.0, 1.0, 1.0)
+	draw_line(from, to, color)
+
+#################
+# Good Code Begin
+#################
 
 # Is the mouse pointer touching the part?
 var is_touched = false
@@ -38,18 +49,31 @@ func _on_mouse_enter():
 func _on_mouse_exit():
 	is_touched = false
 
+func _unhandled_input(event):
+	if is_touched == true:
+		if Input.is_mouse_button_pressed(BUTTON_LEFT):
+			set_global_pos(get_global_mouse_pos())
+
 func _ready():
 	set_mode(MODE_STATIC)
 	set_pickable(true)
 	connect("mouse_enter", self, "_on_mouse_enter")
 	connect("mouse_exit", self, "_on_mouse_exit")
 	set_process_unhandled_input(true)
-	########
-	########
-	print(lowest_y_coord())
-	print(highest_y_coord())
 
-func _unhandled_input(event):
-	if is_touched == true:
-		if Input.is_mouse_button_pressed(BUTTON_LEFT):
-			set_global_pos(get_global_mouse_pos())
+	###############
+	# Good Code End
+	###############
+
+	get_node("DownRay").add_exception(self)
+	get_node("DownRay").set_enabled(true)
+	get_node("DownRay").set_cast_to(Vector2(0.0, lowest_y_coord() + 50.0))
+	set_process(true)
+
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		pass
+	elif get_node("DownRay").is_colliding():
+		print("colliding")
+		var potential_add = get_node("DownRay").get_collider()
+		potential_add.set_pos(Vector2(get_pos().x, get_pos().y + lowest_y_coord() - potential_add.highest_y_coord()))
